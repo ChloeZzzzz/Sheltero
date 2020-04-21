1a00590dd76929c2ad0da70a3fdfa1ecf1ef1e8a
SaintTan <49578914+SaintTan@users.noreply.github.com>
2020-04-22 07:53:51 +1000

    finished modules exercises

--- Notes and Exercises/Workshop 3/Modules/app.js
+++ Notes and Exercises/Workshop 3/Modules/app.js
@@ -7,3 +7,10 @@ const nums=10
 
 console.log(mod1.mod1_string)
 mod2.print_my_mod()
+
+modules.exports = {
+    myfunc: function (){
+        mod1.print_my_mod()
+    },
+    mystring: mod2.mod2_string
+}
--- Notes and Exercises/Workshop 3/Modules/mod2.js
+++ Notes and Exercises/Workshop 3/Modules/mod2.js
@@ -2,7 +2,7 @@ function print_my_mod(){
     console.log("excessing mod 2")
 }
 
-var mod1_string = "Hello Mod2!"
+var mod2_string = "Hello Mod2!"
 
 module.exports.print_my_mod = print_my_mod;
 module.exports.mod1_string = mod1_string;
