643eea9d309d88bd07966db925711176bade34d2
Yifei Zhu <yizhu1@student.unimelb.edu.au>
2020-04-22 13:23:16 +0800

    create user.js
    data temporarily stored in array

---
+++ Project/project app/models/user.js
@@ -0,0 +1,28 @@
+module.exports = [
+      {
+          "id":"10001",
+          "first_name":"Jennifer",
+          "last_name":"Robbins",
+          "gender":"f",
+          "phone_number":"",
+          "email_address":"j_robbins@gmail.com",
+          "credit_points":"1",
+          "credit_level":"1",
+          "company":"",
+          "password" : "1234567",
+          "type":"employee",
+      },
+      {
+          "id":"10002",
+          "first_name":"Evan",
+          "last_name":"Hahn",
+          "gender":"m",
+          "phone_number":"23578965434",
+          "email_address":"ehahahn@gmail.com",
+          "credit_points":"",
+          "credit_level":"",
+          "company":"Sheltero",
+          "password" : "6789012",
+          "type":"employer",
+      }
+  ]
