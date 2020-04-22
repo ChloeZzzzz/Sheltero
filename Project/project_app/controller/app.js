461123fea30208eb081edc7c1324444cd22ce592
dellamou <49134823+dellamou@users.noreply.github.com>
2020-04-22 17:58:42 +0800

    user sign up and login

---
+++ Project/project_app/controller/app.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const bodyParser = require('body-parser');
+const app = express();
+
+const userRouter = require('./routes/userRouter');
+
+app.use(bodyParser.json());
+
+app.use(bodyParser.urlencoded({ extended: true}));
+
+// GET home page
+app.get('/', (req, res) => {
+    res.send('<H1>Sheltero</H1>')
+});
+
+// GET log in page
+app.get('/login', (req, res) => {
+    res.send('<H1>Login page</H1>')
+});
+
+app.use('/login', userRouter);
+
+app.listen(3000, () => {
+    console.log('Sheltero is listening on port 3000!')
+});
