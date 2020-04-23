ba233d5b324e7b7b72165840768de7a0d05ee617
dmou <dmou@student.unimelb.edu.au>
2020-04-23 18:21:03 +0800

    signup and login system by using passport
    also added a simple html page to signup and login

---
+++ Project/models/users.js
@@ -0,0 +1,16 @@
+module.exports = [
+    {
+        "id": "10001",
+        "first_name": "Jennifer",
+        "last_name": "Robbins",
+        "email": "sb@sb",
+        "password": "sunnydog",
+    },
+    {
+        "id": "10002",
+        "first_name": "Evan",
+        "last_name": "Hahn",
+        "email": "md@md",
+        "password": "shabby",
+    }
+]
