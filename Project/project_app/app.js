6337ce956d51302cd70e440134fcd328ccdc9aeb
dmou <dmou@student.unimelb.edu.au>
2020-04-23 20:52:19 +0800

    make the port a variable

--- Project/project_app/app.js
+++ Project/project_app/app.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express();
 
+const PORT = process.env.PORT || 3000;
+app.use(express.static('public'));
+
 // get the user Router for login and signin
 const userRouter = require('./routes/userRouter');
 const jobRouter = require('./routes/jobRouter');
@@ -11,6 +14,6 @@ app.use('/', userRouter)
 
 app.use('/job-search', jobRouter);
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
     console.log('Sheltero is listening on port 3000!')
 });
