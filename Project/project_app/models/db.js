7cf6dd4cdc56657ac48d3dda64f985d4bb2830fa
SaintTan <tanhwaisaint@hotmail.com>
2020-05-06 12:44:11 +1000

    connected to mongodb

---
+++ Project/project_app/models/db.js
@@ -0,0 +1,19 @@
+const {MongoClient} = require("mongodb");
+
+const url = "mongodb+srv://Shetero_20:sheltero20@cluster0-yekum.mongodb.net/test?retryWrites=true&w=majority";
+const client = new MongoClient(url);
+
+async function run(){
+    try {
+        await client.connect();
+        console.log("Connected correctly to server");
+
+    } catch (err) {
+        console.log(err.stack);
+    }
+    finally {
+        await client.close();
+    }
+}
+
+run().catch(console.dir);
