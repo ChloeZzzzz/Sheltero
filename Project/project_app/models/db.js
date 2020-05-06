6327bd20f3906a00d9c133055edf80d2d17b85f7
SaintTan <tanhwaisaint@hotmail.com>
2020-05-06 12:57:39 +1000

    included function for adding, deleting, connecting and disconnecting from db

--- Project/project_app/models/db.js
+++ Project/project_app/models/db.js
@@ -3,17 +3,44 @@ const {MongoClient} = require("mongodb");
 const url = "mongodb+srv://Shetero_20:sheltero20@cluster0-yekum.mongodb.net/test?retryWrites=true&w=majority";
 const client = new MongoClient(url);
 
-async function run(){
-    try {
+async function connnect(){
+    try{
         await client.connect();
-        console.log("Connected correctly to server");
-
-    } catch (err) {
+    }
+    catch(err){
         console.log(err.stack);
     }
-    finally {
-        await client.close();
+}
+
+async function disconnect(){
+    await client.close();
+}
+
+async function insert_data(data, db, collection){
+    try{
+        const db = client.db(db);
+        const col = db.collection(collection);
+
+        const p = await col.insert(data);
+    }
+    catch(err){
+        console.log(err.stack);
     }
 }
+
+async function get_data(db, collection){
+    try{
+        const db = client.db(db);
+        const col = db.collection(collection);
+
+        const myDoc = await col.findOne();
+        return mydoc;
+    }
+    catch(err){
+        console.log(err.stack);
+    }
+}
+
+
 
 run().catch(console.dir);
