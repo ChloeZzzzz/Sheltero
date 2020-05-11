87160a12f15716b400e67437422357541246f299
Yifei Zhu <yizhu1@student.unimelb.edu.au>
2020-05-11 11:26:08 +0800

    changed query method
    .toArray() is not a function (only works on cursor), so changed to res.json

--- Project/project_app/app.js
+++ Project/project_app/app.js
@@ -34,7 +34,7 @@ app.use((req, res, next) => {
 })
 
 app.use((error, req, res, next) => {
-    res.status(err.status || 500);
+    res.status(error.status || 500);
     res.render('error', {
         message: error.message,
         error: error
--- Project/project_app/controllers/jobController.js
+++ Project/project_app/controllers/jobController.js
@@ -4,35 +4,44 @@ var job_data = require('../models/job');
 
 //function for searching all jobs
 const getAllJob = (req, res) => {
-    job_data.find({}, function(err, result) {
-      var jobMap = {};
-
-      jobs.forEach(function(job) {
-        jobMap[job._id] = job;
-      })
-
-      res.send(jobMap);
-    })
+    job_data.find({}, function(err, jobs) {
+        if (err) {
+            res.send("cannot query any data");
+        }
+        res.json(jobs);
+    });
 }
-
-
 
 //function for getting a particular type of jobs
 const getJobByKeyword = (req, res) => {
-    job_data.find({$text: {$search: {"$regex": req.params.keyword}}});
+    job_data.find({$text: {$search: {"$regex": req.params.keyword}}}, function(err, jobs) {
+        if (err) {
+            res.send("cannot query any data for this keyword");
+        }
+        res.json(jobs);
+    });
 }
 
 
 
 //function for getting jobs by a tag
 const getJobByTag = (req, res) => {
-    job_data.find({"jobTag" : req.params.jobTag});
-
+    job_data.find({"jobTag" : req.params.jobTag}, function(err, jobs) {
+        if (err) {
+            res.send("cannot query any data for this jobTag");
+        }
+        res.json(jobs);
+    });
 }
 
 //function for getting jobs by area
 const getJobByArea = (req, res) => {
-    job_data.find({"jobArea" : req.params.jobArea});
+    job_data.find({"jobArea" : req.params.jobArea}, function(err, jobs) {
+        if (err) {
+            res.send("cannot query any data for this jobArea");
+        }
+        res.json(jobs);
+    });
 
 }
 
