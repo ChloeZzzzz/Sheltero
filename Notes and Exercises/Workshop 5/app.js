8435bad20d77bc35508db49a16769b4bde927dd1
SaintTan <tanhwaisaint@hotmail.com>
2020-05-13 12:32:33 +1000

    dynamic port number to app.js

--- Notes and Exercises/Workshop 5/app.js
+++ Notes and Exercises/Workshop 5/app.js
@@ -3,8 +3,6 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
-require("./models");
-
 // use the body-parser middleware, which parses request bodies into req.body
 // support parsing of json
 app.use(bodyParser.json());
@@ -23,8 +21,7 @@ const authorRouter = require("./routes/authorRouter");
 // the author routes are added onto the end of '/author-management'
 app.use("/author-management", authorRouter);
 
-// start app and listen for incoming requests on port
-app.listen(process.env.PORT || 3000, () => {
-  console.log("The library app is running!");
+// start app and listen for incoming requests on port 3000
+app.listen(process.env.PORT||3000, () => {
+  console.log("The library app is listening on port 3000!");
 });
-
--- Notes and Exercises/Workshop 5/models/author.js
+++ Notes and Exercises/Workshop 5/models/author.js
@@ -1,10 +1,15 @@
-const mongoose = require("mongoose");
+// Some static data, an array of objects representing each author
+const authors = [
+  {
+    id: "10001",
+    first_name: "Jennifer",
+    last_name: "Robbins"
+  },
+  {
+    id: "10002",
+    first_name: "Evan",
+    last_name: "Hahn"
+  }
+];
 
-const authorSchema = new mongoose.Schema({
-  id: String,
-  first_name: String,
-  last_name: String
-});
-
-const Author = mongoose.model("author", authorSchema, "author");
-module.exports = Author;
+module.exports = authors;
