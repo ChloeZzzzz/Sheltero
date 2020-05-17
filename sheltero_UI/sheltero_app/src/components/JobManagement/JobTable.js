8746fe80715541f1a1ff2fd59dd9b784e7ad3204
vivi7148 <61859585+vivi7148@users.noreply.github.com>
2020-05-17 12:41:58 +1000

    update JobSearch.js, api.js, add JobTable

---
+++ sheltero_UI/sheltero_app/src/components/JobManagement/JobTable.js
@@ -0,0 +1,73 @@
+import React from "react";
+
+import { getJobs } from "../../api";
+import {
+  Card,
+  CardBody,
+  CardFooter,
+  CardImageHeader,
+  CardText,
+  CardTitle
+} from "styled-card-component";
+import { Button } from 'styled-button-component';
+import { Column, Row } from 'styled-grid-system-component';
+
+export class JobTable extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      jobs: []
+    };
+  }
+
+  async fetchJobs() {
+    const data = await getJobs();
+    this.setState({ jobs: data, isLoaded: true });
+  }
+
+  componentDidMount() {
+    this.fetchJobs();
+  }
+
+  render() {
+    const { error, isLoaded, jobs } = this.state;
+    if (error) {
+      return <div> ERROR:{error.message}</div>;
+    } else if (!isLoaded) {
+      return <div>Loading..</div>;
+    } else {
+      return (
+        <div>
+          <Row >
+          
+          {jobs.map((value, index) => {
+            return (<Column xl={6} >
+            
+              <Card active key={index} >
+                <CardImageHeader key={index} src={value.jobImg} />
+                <CardBody>
+                  <CardTitle>{value.jobTitle}</CardTitle>
+                  <CardText>{value.jobDetail}</CardText>
+                  <CardFooter key={index}>
+                    Credit_level:{value.credit_level}
+                  </CardFooter>
+                  <Button primary style={{
+                    backgroundColor:"#99C015", 
+                    borderColor:"#99C015",
+                    fontSize: "14px"}}>
+                      More Info
+                      </Button>
+                </CardBody>
+              </Card></Column>
+
+            );
+          })}
+          
+        </Row>
+        </div>
+      );
+    }
+  }
+}
---
+++ sheltero_UI/sheltero_app/src/components/JobSearch.js
@@ -0,0 +1,82 @@
+import React from "react";
+import Button from "@material-ui/core/Button";
+import Card from "@material-ui/core/Card";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Grid from "@material-ui/core/Grid";
+import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
+import SearchByCategory from "../components/SearchBar.js";
+import { Box } from "@material-ui/core";
+import SearchByArea from "../components/SearchArea";
+import {JobTable} from "../components/JobManagement/JobTable";
+import Copyright from "../components/Copyright";
+
+const useStyles = makeStyles(theme => ({
+  icon: {
+    marginRight: theme.spacing(2)
+  },
+
+  heroButtons: {
+    marginTop: theme.spacing(4)
+  },
+  cardGrid: {
+    paddingTop: theme.spacing(8),
+    paddingBottom: theme.spacing(8)
+  },
+  card: {
+    height: "100%",
+    display: "flex",
+    flexDirection: "column"
+  },
+  cardMedia: {
+    paddingTop: "56.25%" // 16:9
+  },
+  cardContent: {
+    flexGrow: 1
+  },
+  footer: {
+    backgroundColor: theme.palette.background.paper,
+    padding: theme.spacing(6)
+  },
+  Box: {
+    width: "100vw",
+    backgroundColor: theme.palette.background.paper,
+    padding: theme.spacing(8, 0, 6)
+  }
+}));
+
+export default function Job() {
+  const classes = useStyles();
+
+  return (
+    <React.Fragment>
+      <CssBaseline />
+
+      <main>
+        <div className={classes.Box}>
+          <Grid container spacing={12}>
+            <Grid xs={1} />
+            <Grid item xs={4}>
+              <SearchByCategory />
+            </Grid>
+            <Grid xs={1} />
+            <Grid item xs={4}>
+              <SearchByArea />
+              {/* <SearchLocationInput onChange={() => null} /> */}
+            </Grid>
+          </Grid>
+        </div>
+        <Container maxWidth="md">
+          <JobTable />
+        </Container>
+      </main>
+
+      <Box mt={5} className={classes.box}>
+        <Copyright />
+      </Box>
+    </React.Fragment>
+  );
+}
