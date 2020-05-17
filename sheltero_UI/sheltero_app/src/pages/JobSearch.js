701791aeeaa91991ed9b69713f94763630e577f5
vivi <wenyic1@student.unimelb.edu.au>
2020-05-17 14:49:56 +1000

    update

--- sheltero_UI/sheltero_app/src/pages/JobSearch.js
+++ sheltero_UI/sheltero_app/src/pages/JobSearch.js
@@ -1,151 +1,77 @@
-import React,{ useState, useRef } from 'react';
-import Button from '@material-ui/core/Button';
-import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import Grid from '@material-ui/core/Grid';
-import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
-import Container from '@material-ui/core/Container';
-import SearchByCategory from '../components/SearchBar.js';
-import { Link, Box } from '@material-ui/core';
-//import SectionCarousel from '../components/Carousel/Carousel';
-import SearchByArea from '../components/SearchArea';
-import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-    useParams
-} from "react-router-dom";
+import React from "react";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Grid from "@material-ui/core/Grid";
+import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
+import SearchByCategory from "../components/SearchBar.js";
+import { Box } from "@material-ui/core";
+import SearchByArea from "../components/SearchArea";
+import {JobTable} from "../components/JobManagement/JobTable";
+import Copyright from "../components/Copyright";
 
-const useStyles = makeStyles((theme) => ({
-    icon: {
-        marginRight: theme.spacing(2),
-    },
+const useStyles = makeStyles(theme => ({
+  icon: {
+    marginRight: theme.spacing(2)
+  },
 
-    heroButtons: {
-        marginTop: theme.spacing(4),
-    },
-    cardGrid: {
-        paddingTop: theme.spacing(8),
-        paddingBottom: theme.spacing(8),
-    },
-    card: {
-        height: '100%',
-        display: 'flex',
-        flexDirection: 'column',
-    },
-    cardMedia: {
-        paddingTop: '56.25%', // 16:9
-    },
-    cardContent: {
-        flexGrow: 1,
-    },
-    footer: {
-        backgroundColor: theme.palette.background.paper,
-        padding: theme.spacing(6),
-    },
-    Box: {
-        width:'100vw',
-        backgroundColor: theme.palette.background.paper,
-        padding: theme.spacing(8, 0, 6),
-
-    }
+  heroButtons: {
+    marginTop: theme.spacing(4)
+  },
+  cardGrid: {
+    paddingTop: theme.spacing(8),
+    paddingBottom: theme.spacing(8)
+  },
+  card: {
+    height: "100%",
+    display: "flex",
+    flexDirection: "column"
+  },
+  cardMedia: {
+    paddingTop: "56.25%" // 16:9
+  },
+  cardContent: {
+    flexGrow: 1
+  },
+  footer: {
+    backgroundColor: theme.palette.background.paper,
+    padding: theme.spacing(6)
+  },
+  Box: {
+    width: "100vw",
+    backgroundColor: theme.palette.background.paper,
+    padding: theme.spacing(8, 0, 6)
+  }
 }));
 
-const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-
 export default function Job() {
-    const classes = useStyles();
-    const node = useRef();
+  const classes = useStyles();
 
-    return (
-        <React.Fragment>
-            <CssBaseline />
-            {/*<SectionCarousel/>*/}
-            <main>
-                {/* Hero unit */}
-                <div  className={classes.Box}>
-                    <Grid container spacing={12}>
-                      <Grid xs={1} />
-                      <Grid item xs={4} >
-                        <SearchByCategory/>
-                      </Grid>
-                      <Grid xs={1} />
-                      <Grid item xs={4}>
-                        <SearchByArea/>
-                        {/* <SearchLocationInput onChange={() => null} /> */}
-                      </Grid>
-                    </Grid> 
-                </div>
-                <Container className={classes.cardGrid} maxWidth="md">
-                    {/* End hero unit */}
-                    <Grid container spacing={4}>
-                        {cards.map((card) => (
-                            <Grid item key={card} xs={12} sm={6} md={4}>
-                                <Card className={classes.card}>
-                                    <CardMedia
-                                        className={classes.cardMedia}
-                                        image="https://source.unsplash.com/random"
-                                        title="Image title"
-                                    />
-                                    <CardContent className={classes.cardContent}>
-                                        <Typography gutterBottom variant="h5" component="h2">
-                                            Job Title
-                                        </Typography>
-                                        <Typography>
-                                            There is description of the job
-                                        </Typography>
-                                    </CardContent>
-                                    <CardActions>
-                                        <Button size="small" color="transparent">
-                                            Details
-                                        </Button>
-                                        <Button size="small" color="transparent">
+  return (
+    <React.Fragment>
+      <CssBaseline />
 
-                                        </Button>
-                                    </CardActions>
-                                </Card>
-                            </Grid>
-                        ))}
-                    </Grid>
-                </Container>
-            </main>
-            {/* Footer */}
-            <Box mt={5} className={classes.box}>
-                <Copyright />
-            </Box>
-            {/* End footer */}
-        </React.Fragment>
-        // <Router>
-        //     <Switch>
-        //         <Route exact path="/:jobID">
-        //             <JobManagement/>
-        //         </Route>
-        //     </switch>
-        // </Router>
+      <main>
+        <div className={classes.Box}>
+          <Grid container spacing={12}>
+            <Grid xs={1} />
+            <Grid item xs={4}>
+              <SearchByCategory />
+            </Grid>
+            <Grid xs={1} />
+            <Grid item xs={4}>
+              <SearchByArea />
+              {/* <SearchLocationInput onChange={() => null} /> */}
+            </Grid>
+          </Grid>
+        </div>
+        <Container maxWidth="md">
+          <JobTable />
+        </Container>
+      </main>
 
-
-
-
-    );
-}
-
-function Copyright() {
-    return (
-      <Typography variant="body2" color="textSecondary" align="center">
-        {'Copyright © '}
-        <Link color="inherit" href="/">
-          SHELTERO.
-        </Link>{' '}
-        {new Date().getFullYear()}
-        {'.'}
-      </Typography>
-    );
-}
-
-function JobManagement(){
-    let{ jobID }=useParams();
+      <Box mt={5} className={classes.box}>
+        <Copyright />
+      </Box>
+    </React.Fragment>
+  );
 }
