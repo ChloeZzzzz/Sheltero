61b69606c5a8816283d81244282f75ce9c777040
vivi7148 <61859585+vivi7148@users.noreply.github.com>
2020-06-03 19:45:45 +1000

    Update About.js

--- sheltero_UI/sheltero_app/src/pages/About/About.js
+++ sheltero_UI/sheltero_app/src/pages/About/About.js
@@ -38,16 +38,6 @@ export default function AboutPage(props) {
                 impression.
               </h4>
               <br />
-              <Button
-                color="danger"
-                size="lg"
-                href="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ref=creativetim"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <i className="fas fa-play" />
-                Watch video
-              </Button>
             </GridItem>
           </GridContainer>
         </div>
