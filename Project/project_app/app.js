7b2eb7ae43c7eaca4dc71cebc775d1d0532b279d
Danlei Mou <dmou@student.unimelb.edu.au>
2020-06-05 00:43:59 +0800

    cors

--- Project/project_app/app.js
+++ Project/project_app/app.js
@@ -24,7 +24,7 @@ const userRouter = require('./routes/userRouter');
 const jobRouter = require('./routes/jobRouter');
 
 app.use(function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Origin', 'http://sheltero.herokuapp');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     next();
 });
