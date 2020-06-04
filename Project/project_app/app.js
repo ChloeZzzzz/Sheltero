8445e5db85c0912f6c7df2cdfe470ce1bcdd8dd9
Danlei Mou <dmou@student.unimelb.edu.au>
2020-06-04 21:50:09 +0800

    update origin to sheltero

--- Project/project_app/app.js
+++ Project/project_app/app.js
@@ -14,7 +14,7 @@ const session = require("express-session");
 
 app.use(cors({
     credentials:true,
-    origin: '^(https?://(?:.+\.)?herokuapp\.com(?::\d{1,5})?)$',
+    origin: 'http://sheltero.herokuapp.com/',
 }))
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
