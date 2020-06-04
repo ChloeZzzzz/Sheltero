2941decdb69161ae69fe52498e69b20e07f231b1
Danlei Mou <dmou@student.unimelb.edu.au>
2020-06-05 00:35:43 +0800

    add access-control-allow-orign

--- Project/project_app/app.js
+++ Project/project_app/app.js
@@ -23,6 +23,12 @@ app.set('view engine', 'ejs');
 const userRouter = require('./routes/userRouter');
 const jobRouter = require('./routes/jobRouter');
 
+app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+});
+
 app.use(morgan('dev'));
 
 app.use('/user', userRouter);
