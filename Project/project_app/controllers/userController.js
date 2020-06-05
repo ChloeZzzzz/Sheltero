5ce7681add7aa91e0d24b0dbcc1c2a799ac80508
SaintTan <tanhwaisaint@hotmail.com>
2020-06-05 15:02:52 +1000

    sending user info

--- Project/project_app/controllers/userController.js
+++ Project/project_app/controllers/userController.js
@@ -8,34 +8,29 @@ const getUserHomepage = async (req, res) => {
     let session = req.session;
     if (session.passport.user) {
         let user = await Users.findById(user.passport.user);
+        req.flash({"message": "User Info:"})
         res.json(user);
         return res.end();
+    }
+    else{
+        res.json({"message":"Invalid User :("});
+        return res.end()
     }
 }
 
 const successSignup = (req, res) => {
-    if (req.session) {
-        res.json(req.session);
-        return res.end();
-    } else {
-        res.json(req.session);
-        return res.end();
-    }
+    res.json(req.session);
+    return res.end();
 }
 
 const failureSignup = (req, res) => {
-    res.json("failureSignup")
+    res.json(req.session)
     return res.end();
 }
 
 const successLogin = (req, res) => {
-    if (req.session) {
-        res.json(req.session);
-        return res.end();
-    } else {
-        res.json(req.session);
-        return res.end();
-    }
+    res.json(req.session);
+    return res.end();
 }
 
 const failureLogin = (req, res) => {
@@ -62,70 +57,6 @@ const getUserLogout = (req, res) => {
     } else {
         res.redirect('../')
     }
-}
-
-// post user sign up
-// -> encrypt the user's password before store it into the database for security issues
-// -> has to use async since have to wait for encryption completet
-const postUserSignup = async (req, res) => {
-    console.log(req);
-    try {
-        if (await email_validator.validate(req.body.email) && await emailNotSignedUp(req.body.email)) {
-            const cryptedpw = await bcrypt.hash(req.body.password, 10);
-            const user = new Users({
-                "_id" : new mongoose.Types.ObjectId(),
-                "gender" : req.body.gender,
-                "first_name" : req.body.first_name,
-                "last_name": req.body.last_name,
-                "email" : req.body.email,
-                "password" : cryptedpw,
-                "contact": req.body.contact,
-                "company_name": req.body.company_name,
-                "company_addr": req.body.company_addr,
-                "type" : req.body.type,
-                "resume": {job: 'programmer'}
-            });
-
-            user.save().then(result => {
-                console.log("== SAVED TO DATABASE ==")
-                console.log(result);
-            }).catch(err => {
-                console.log(user);
-                console.log(err);
-            });
-
-            res.json('success');
-        } else {
-            console.log("Invalid email or email already signed up!");
-            res.json('fail: invalid email or email already signed up');
-            //res.redirect('signup');
-        }
-
-    } catch (e) {
-        console.log("Failed to Sign up");
-        console.log(e);
-        res.json('fail')
-        //res.redirect('signup');
-    }
-}
-
-// a helper function, checks whether this email is exist in the database
-const emailNotSignedUp = async (email) => {
-    const user = await Users.findOne({'email': email}, (err, result) => {
-        console.log("inside find function");
-        if (err) {
-            console.log("==ERROR==");
-            console.log(err);
-        }
-        console.log("==RESULT==");
-        console.log(result);
-    })
-    console.log("==USER==");
-    console.log(user);
-    if (user == null) {
-        return true;
-    }
-    return false;
 }
 
 const postUpdateUser = async(req, res) => {
@@ -168,7 +99,6 @@ module.exports = {
     getUserSignup,
     getUserLogin,
     getUserLogout,
-    postUserSignup,
     //postUpdateUser,
     successLogin,
     failureLogin,
