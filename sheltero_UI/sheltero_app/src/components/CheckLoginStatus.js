27ac0399d3c0388bf34e3249f3244e9132a76310
SaintTan <tanhwaisaint@hotmail.com>
2020-06-06 07:56:06 +1000

    implementing new detection for cookies

--- sheltero_UI/sheltero_app/src/components/CheckLoginStatus.js
+++ sheltero_UI/sheltero_app/src/components/CheckLoginStatus.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Cookies from 'js-cookie';
 import axios from "axios";
 
 export default class CheckLogin extends React.Component {
@@ -13,41 +14,13 @@ export default class CheckLogin extends React.Component {
       this.handleLogout = this.handleLogout.bind(this);
     }
     
-    checkLoginStatus() {
-      axios.get("http://shelteroinf.herokuapp.com/user", {withCredentials: true, crossdomain:true})
-        .then(res => {
-          console.log(res);
-          if ( // the case when response data is logged in but the current state is not logged in
-            res.data.login && this.state.loggedInStatus === false
-          ) {
-            this.setState({
-              loggedInStatus: true,
-              user: res.data.user
-            });
-          } else if ( // the case when the current state is logged in but the user is not actually authenticated
-            !res.data.login & (this.state.loggedInStatus === true)
-          ) {
-            this.setState({
-              loggedInStatus: true,
-              user: {}
-            });
-          }
-        })
-        .catch(error => {
-          console.log("check login error", error);
-        });
-    }
-
-    componentDidMount() { //componentDidMount() is invoked immediately after a component is mounted
-      //this.checkLoginStatus();
-    }
     handleLogout(){
       this.setStatus({
         loggedInStatus:false,
         // user: {}
       });
     }
-    handleLogin(data) {
+    handleLogin() {
       this.setState({
         loggedInStatus: true,
         // user: data.user
