2e9adf69449c1676d1582f76c4b763f663415509
SaintTan <tanhwaisaint@hotmail.com>
2020-06-07 01:25:44 +1000

    checking update information

--- Project/project_app/controllers/userController.js
+++ Project/project_app/controllers/userController.js
@@ -55,7 +55,8 @@ const getUserLogout = (req, res) => {
 }
 
 const postUpdateUser = async(req, res) => {
-    console.log(req);
+    console.log(req.body);
+    console.log(req.params);
     if (req.user) {
         if (req.body.contact != null) {
             userData.contact = req.body.contact;
